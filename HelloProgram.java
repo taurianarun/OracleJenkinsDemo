54a57a764e81e2a6fdf3ce04250074f1f237e466
vishymails <vishymails@gmail.com>
2020-10-14 15:39:17 +0530

    Hello Program

---
+++ HelloProgram.java
@@ -0,0 +1,9 @@
+public class HelloProgram {
+    public static void main(String[] s){
+        System.out.println("Loop based execution");
+
+        for(int i=0; i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
